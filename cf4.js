89553a20d9fc7976731af376c4bd041c2d471839
nathias <n@th1.as>
2018-11-05 17:01:01 +0100

    fourth

---
+++ cf4.js
@@ -0,0 +1,13 @@
+var ninja;
+
+function Ninja() {
+  this.swung = true;
+}
+
+ninja = new Ninja();
+
+Ninja.prototype.swingSword = function() {
+  return this.swung;
+};
+
+console.log(ninja.swingSword());
