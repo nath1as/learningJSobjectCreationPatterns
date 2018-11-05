5ea322b496405ef31b409ca48937efbabc9de4aa
nathias <n@th1.as>
2018-11-05 13:41:11 +0100

    second

---
+++ pi2.js
@@ -0,0 +1,30 @@
+function shallowCopy(object) {
+  var result = Object.create(Object.getPrototypeOf(object));
+  var prop;
+
+  for (prop in object) {
+    if (Object.prototype.hasOwnProperty.call(object, prop)) {
+      result[prop] = object[prop];
+    }
+  }
+
+  return result;
+}
+
+var foo = {
+  a: 1,
+  b: 2,
+};
+
+var bar = Object.create(foo);
+bar.c = 3;
+bar.say = function() {
+  console.log('c is ' + this.c);
+};
+
+var baz = shallowCopy(bar);
+console.log(baz.a);       // 1
+baz.say();                // "c is 3"
+baz.hasOwnProperty('a');  //false
+baz.hasOwnProperty('b');  //false
+
