c820f50afedb17a43e72f78ddf6488849db631fc
nathias <n@th1.as>
2018-11-06 13:27:37 +0100

    frozen objects

---
+++ freeze.js
@@ -0,0 +1,29 @@
+var frozen = {
+  integer: 4,
+  string: 'String',
+  array: [1, 2, 3],
+  object: {
+    foo: 'bar'
+  },
+  func: function() {
+    console.log('I\'m frozen');
+  },
+};
+
+Object.freeze(frozen);
+frozen.integer = 8;
+frozen.string = 'Number';
+frozen.array.pop();
+frozen.object.foo = 'baz';
+frozen.func = function() {
+  console.log('I\'m not really frozen');
+};
+
+console.log(frozen.integer);      // 4
+console.log(frozen.string);       // String
+console.log(frozen.array);        // [1, 2]
+console.log(frozen.object.foo);   // baz
+frozen.func();                    // I'm frozen
+
+// for prop values that are objects the references to the objects are frozen
+// // you can still use the frozen references to mutate the objects
